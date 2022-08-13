aa8ae4066c11a6474b75393af4541a377761ef79
hamsa <hafieekhan@gmail.com>
2022-08-14 00:08:16 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(arr, message){
+    let cards = [];
+    for(let i = 0; i<arr.length; i++){
+        cards.push(`Thank you, ${arr[i]}, for the wonderful ${message} gift!`);
+          
+    }
+    return cards;
+}
+ function countDown(n){
+    while(n>=0){
+        console.log(n);
+        n--;
+    }
+ 
+ }
